f3dd83281451709ef21fb688aa0391cf5625b11b
Karolis Užkuraitis <karolis.uzkuraitis+github@gmail.com>
2018-01-13 22:50:25 +0200

    Added test with number "2"

--- mnist1 - Added class test.py
+++ mnist1 - Added class test.py
@@ -29,11 +29,25 @@ test_labels = np.full(1200, 10, dtype=int)
 train_data = train_data.reshape(train_data.shape[0], img_rows, img_cols, 1)
 test_data = test_data.reshape(test_data.shape[0], img_rows, img_cols, 1)
 
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+
 model = load_model("model-mnist1 - 1.h5")
 
 # score = model.evaluate(x_test, y_test, verbose=0)
 # print('Test loss:', score[0])
 # print('Test accuracy:', score[1])
+
+
+target_test_image = x_test[1]
+
+plt.imshow(target_test_image.reshape(img_rows, img_cols))
+plt.show()
+
+results = model.predict(target_test_image.reshape(1, img_rows, img_cols, 1))
+print("predictions with test number - ")
+print(results)
+print(model.predict_classes(target_test_image.reshape(1, img_rows, img_cols, 1)))
 
 target_test_image = test_data[1]
 
